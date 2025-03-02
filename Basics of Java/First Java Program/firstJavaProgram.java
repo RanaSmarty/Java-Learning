acddeeb2b13e17f60e8f515445a11e2fb2d0d5e2
Rana Smarty <ranasmarty00@gmail.com>
2025-03-03 00:03:30 +0600

    First of all added a directory named 'First Java Program' and also added a file according to this directory name as 'firstJavaProgram.java'.

---
+++ Basics of Java/First Java Program/firstJavaProgram.java
@@ -0,0 +1,7 @@
+public class firstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hurrey! this is my first program in Java language.");
+        return;
+    }
+}
