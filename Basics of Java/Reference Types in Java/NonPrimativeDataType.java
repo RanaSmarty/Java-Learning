766115a19b3dad04ec4b87e8daf1135f0aab8190
Rana Smarty <ranasmarty00@gmail.com>
2025-03-04 12:55:43 +0600

    Added a new file nemed 'NonPrimativeDataType.java' in Reference folder or directory...

---
+++ Basics of Java/Reference Types in Java/NonPrimativeDataType.java
@@ -0,0 +1,33 @@
+public class NonPrimativeDataType {
+
+    public static void main(String[] args) {
+        
+        // String type of data type
+        String greetingUser = "Asslamu Alaiqum Wa-Rohmatullah!";
+        String greetWorld = "Hello World!";
+        String greetJava = "Hey, Java!";
+
+        // Array type data type
+        int [] numbers = {1,2, 3, 4, 5};
+
+        // Parallel arrays type of data type
+        String nameOfPerson = "Rana Smarty";
+        int ageOfPerson = 21;
+        double heightPerson = 5.10;
+
+        // Displaying to the console...
+        System.out.println(greetingUser);
+        System.out.println("Numbers: ");
+        
+        for (int number : numbers) {
+            System.out.println(number + "");
+        }
+        System.out.println();
+
+        System.out.println("Person Name: " + nameOfPerson + ".");
+        System.out.println("Age: " + ageOfPerson + ".");
+        System.out.println("Height: " + heightPerson + "Feet.");
+
+        return;
+    }
+}
