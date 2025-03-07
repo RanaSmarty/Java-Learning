f451a11b38c0f494d8869ead81e16721cccc6474
Rana Smarty <ranasmarty00@gmail.com>
2025-03-08 01:38:48 +0600

    Modified and clarified 'TestIdentifiers' codes and commit

--- Basics of Java/Identifiers/TestIdentifiers.java
+++ Basics of Java/Identifiers/TestIdentifiers.java
@@ -5,9 +5,18 @@ public class TestIdentifiers {
     public static void main(String[] args) {
         
         int number = 10;
+        int age = 21;
+        /*
+         * 'number' and 'age' is valid identifiers
+         */
+
+        String firstName = "Rana"; // 'firstName' folllows Java naming conventions
         
         // unnecessary but no problem! 
+        System.out.println("First Name: " + firstName);
+        System.out.println("Age: " + age);
         System.out.println("Number: " + number);
+        
         return;
     }
 }
