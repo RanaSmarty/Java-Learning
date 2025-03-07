d48893f6976379301c68628a69466a0525545135
Rana Smarty <ranasmarty00@gmail.com>
2025-03-07 22:33:21 +0600

    Added 'TestIdentifiers' file

--- Basics of Java/Identifiers/TestIdentifiers.java
+++ Basics of Java/Identifiers/TestIdentifiers.java
@@ -1,10 +1,13 @@
 package Identifiers;
 
-class TestIdentifiers {
+public class TestIdentifiers {
 
     public static void main(String[] args) {
         
         int number = 10;
+        
+        // unnecessary but no problem! 
+        System.out.println("Number: " + number);
         return;
     }
 }
