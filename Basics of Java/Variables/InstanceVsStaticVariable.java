75fc03714d399498fa4013881d4ca2d9f7c9ba3e
Rana Smarty <ranasmarty00@gmail.com>
2025-03-12 02:35:51 +0600

    Added Java program that demonstrate difference between Instance and Static Variables...

---
+++ Basics of Java/Variables/InstanceVsStaticVariable.java
@@ -0,0 +1,29 @@
+class InstanceVsStaticVariable {
+    // Declared an Instance variable and stored value...
+    int instanceVariable = 21;
+
+    // Declared a static variable;
+    static int staticVariable = 22;
+
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        
+        InstanceVsStaticVariable obj1 = new InstanceVsStaticVariable();
+        InstanceVsStaticVariable obj2 = new InstanceVsStaticVariable();
+
+        obj1.instanceVariable += 4;  // Changes obj1's instance variable 25
+        obj2.staticVariable += 4;     // Changes the static variable 26 and created this object for just understading not essential;
+
+        System.out.println("Object 1 - Instance Variable: " + obj1.instanceVariable);
+       // An extra example for Instance Variable... (obj2) but essential;
+       System.out.println("Object 2 - Instance variable: " + obj2.instanceVariable);
+       
+       System.out.println();
+       System.out.println("Difference...");
+       System.out.println();
+       
+       // Static variable is shared across all instances
+       System.out.println("Static Variable: " + InstanceVsStaticVariable.staticVariable);
+
+    }
+}
