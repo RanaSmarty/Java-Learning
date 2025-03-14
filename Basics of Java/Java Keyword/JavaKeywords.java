f7bcb3410c6d3a93518d65445e4893ddf65a3c76
Rana Smarty <ranasmarty00@gmail.com>
2025-03-15 03:47:58 +0600

    Add Detailed Explanation of Java Keywords
    - Included comprehensive description of Java Keywords and Reserved Words.
    - Provided examples demonstrating keyword usage and compile-time errors.
    - Categorized keywords by usage:
      - Data Types & Variables
      - Class & Object Handling
      - Control Statements
      - Exception Handling
      - Access Modifiers & Packages
      - Modifiers & Non-Access Modifiers
      - Special Keywords
      - Reserved Keywords (Unused)
      - Literals (Not Keywords)
     - Highlighted important points about keywords.

---
+++ Basics of Java/Java Keyword/JavaKeywords.java
@@ -0,0 +1,12 @@
+// Java program to illustrate what if we use the keywords as the variable name :)
+
+/*public class JavaKeywords {
+    // Main Function
+    public static void main(String[] args) {
+        
+        // Note 'this' is a reserved word in Java
+        String this = "Hello Java Keywords!";
+        System.out.println(this);
+    }
+}
+*/
