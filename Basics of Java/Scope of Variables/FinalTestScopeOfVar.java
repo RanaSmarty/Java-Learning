d00e31557d86ebb761ded580bb26bc8853f66566
Rana Smarty <ranasmarty00@gmail.com>
2025-03-16 03:02:36 +0600

    Add FinalTestScopeOfVar Class to Demonstrate Static and Instance Variable Scope
    - Implemented the FinalTestScopeOfVar class to demonstrate the usage of static and instance variables.
    - Showed how to modify and access static (FinalTestScopeOfVar.x) and instance variables (ScopeVariable.y).
    - Demonstrated the usage of 	his keyword to modify instance and static fields.

---
+++ Basics of Java/Scope of Variables/FinalTestScopeOfVar.java
@@ -0,0 +1,20 @@
+public class FinalTestScopeOfVar {
+
+    static int x = 21;
+    private int y = 25;
+
+    public void method1(int x) {
+        FinalTestScopeOfVar ScopeVariable = new FinalTestScopeOfVar();
+        this.x = 22; // Modifies the static field x
+        this.y = 30; // Modifies the instance field y
+
+        // Printing the static and instance fields correctly
+        System.out.println("Static FinalTestScopeOfVar.x : " + FinalTestScopeOfVar.x);
+        System.out.println("Instance ScopeVariable.y : " + ScopeVariable.y);
+    }
+
+    public static void main(String[] args) {
+        FinalTestScopeOfVar ScopeVariable = new FinalTestScopeOfVar();
+        ScopeVariable.method1(5); // Calling the method using an instance
+    }
+}
